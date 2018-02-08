0be40e3caacdc6ec3b8ff209d89ab3e5621c98c2
pds54 <35676624+pds54@users.noreply.github.com>
2018-02-08 17:41:55 -0600

    Update add.sh

--- proj1/add.sh
+++ proj1/add.sh
@@ -4,3 +4,38 @@
 #check if db.txt exists & otherwise create and populate
 
 echo -e "let's add a record!\n";
+
+NAME=":"
+ADDRESS=":"
+PHONE_NUMBER=":"
+EMAIL=":"
+
+while [[ $NAME == *":"* ]]
+do
+  echo Name cannot contain a colon
+  echo What is your name?
+  read NAME
+done
+
+while [[ $ADDRESS == *":"* ]]
+do
+  echo Address cannot contain a colon
+  echo What is your address?
+  read ADDRESS
+done
+
+while [[ $PHONE_NUMBER == *":"* ]]
+do
+  echo Phone number cannot contain a colon
+  echo What is your phone number?
+  read PHONE_NUMBER
+done
+
+while [[ $EMAIL == *":"* ]]
+do
+  echo Email cannot contain a colon
+  echo What is your email?
+  read EMAIL
+done
+
+echo $NAME:$ADDRESS:$PHONE_NUMBER:$EMAIL >> database.txt
