0c9dabf84241345d87dc8fc1a0f89d2215fed0fe
wcj1 <36248206+wcj1@users.noreply.github.com>
2018-02-11 02:30:56 -0600

    Update update.sh

--- proj1/update.sh
+++ proj1/update.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 # Update functionality
 
+echo -e "What would you like to update?\n"
+
+echo -e " (a) Name\n" "(b) Address\n" "(c) Phone Number\n" "(d) Email\n"
+
+read input
+
+	case $input
+		"a"|"A"|"1")
+			read -p "Input current Name\n" var;;
+		"b"|"B"|"2")
+			read -p "Input current Address\n" var;;
+		"c"|"C"|"3")
+			read -p "Input current Phone Number\n" var;;
+		"d"|"D"|"4")
+			read -p "Input current Email\n" var;;
+			break;;
+		*) echo -e '\t\t('!') INVALID CHOICE ('!')';;
+		
+read -p "What would you like to change it to?\n" new_Input	
+
+sed 's/$var/$new_Input/' database.txt
+
 echo -e "updating existing record..\n"
