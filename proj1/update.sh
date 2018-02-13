dd9522e85879cccdf1b62264379795d8183102d0
wcj1 <36248206+wcj1@users.noreply.github.com>
2018-02-13 16:29:46 -0600

    Update update.sh

--- proj1/update.sh
+++ proj1/update.sh
@@ -9,20 +9,37 @@ read input
 
 	case $input in
 		"a"|"A"|"1")
-			read -p "Input current Name\n" var;;
+			read -p "Input current Name\n" NAME
+			if [grep -F "$NAME"]; then 
+				read -p "What would you like to change it to?\n" new_var
+			else
+				echo "No one by that name in this database.\n"
+			fi;;
 		"b"|"B"|"2")
-			read -p "Input current Address\n" var;;
+			read -p "Input current Address\n" ADDRESS
+			if [grep -F "$ADDRESS"]; then 
+				read -p "What would you like to change it to?\n" new_var
+			else
+				echo "No one with that address in this database.\n"
+			fi;;
 		"c"|"C"|"3")
-			read -p "Input current Phone Number\n" var;;
+			read -p "Input current Phone Number\n" PHONE_NUMBER
+			if [grep -F "$PHONE_NUMBER"]; then 
+				read -p "What would you like to change it to?\n" new_var
+			else
+				echo "No one with that phone number in this database.\n"
+			fi;;
 		"d"|"D"|"4")
-			read -p "Input current Email\n" var;;
+			read -p "Input current Email\n" EMAIL
+			if [grep -F "$NAME"]; then 
+				read -p "What would you like to change it to?\n" new_var
+			else
+				echo "No one with that email in this database.\n"
+			fi;;
 			break;;
-		*) echo -e '\t\t('!') INVALID CHOICE ('!')';;
-	
+		*) echo -e '\t\t('!') INVALID CHOICE ('!')';;	
 	esac
-		
-read -p "What would you like to change it to?\n" new_Input	
 
-sed 's/$var/$new_Input/' database.txt
+#$grep -v "$var" database.txt >> updated_db.txt
 
 echo -e "updating existing record..\n"
