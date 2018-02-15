d49a8915353c93d9f7197b7ba36855a23c372a05
david crowe <dac183@txstate.edu>
2018-02-15 16:46:04 -0600

    Handles some phone number and email edge cases

--- proj1/search.sh
+++ proj1/search.sh
@@ -9,76 +9,82 @@ line1=":"
 fields="x"
 input=""
 minEmailCount=5
+minPhoneDigits=7
 until [[ $fields =~ ^[a-d]*$ ]]
 	do 
     clear
-    echo -e "Which contact fields would you like to search?\n(*) Enter up to 4 choices with no spaces (e.g. abd) (*) \n";
+    echo -e "Which contact fields would you like to search?\nEnter up to 4 choices with no spaces (e.g. abd) \n";
     echo -e " (a) Name \n (b) Address \n (c) Phone Number \n (d) E-Mail \n";
     read -p "Enter choice: " fields
 done
 
-for f in $fields 
+for (( f=0; f<${#fields}; f++));
 	do
 	#choice=$f
-	#echo -e "${#fields} \n";
-	case $f in
+	choice=${fields:$f:1}
+	case $choice in
 		"a"|"A"|"1")
             while [[ $NAME == *":"* ]]
             	do
-                echo "Search for contact name: "
-                read NAME
+                read -p "Search for contact name: " NAME
             done;;
 
 		"b"|"B"|"2")
             while [[ $ADDRESS == *":"* ]]
                 do
-                echo "Search for contact address: "
-                read ADDRESS
+                read -p "Search for contact address: " ADDRESS
                 done;;
 
 		"c"|"C"|"3")
-            while [ $PHONE_NUMBER == *":"* ]  && [ ${#PHONE_NUMBER} -gt 7 ]
+            while [[ $PHONE_NUMBER == *":"* ]]  || [[ ${#PHONE_NUMBER} -lt $minPhoneDigits ]]
                 do
-                echo "Search for contact phone number: " 
-                read PHONE_NUMBER
+                read -p "Search for contact phone number (at least ${minPhoneDigits} digits): " PHONE_NUMBER
                 done;;
                 
 		"d"|"D"|"4")
-            #while [[ $EMAIL == *":"* ] && [ ${#EMAIL} -lt $minEmailCount ]]
-            while [[ $EMAIL == *":"* ]]
+            while [[ $EMAIL == *":"* ]] || [[ ${#EMAIL} -lt $minEmailCount ]]
+            #while [[ $EMAIL == *":"* ]]
                 do
-                echo "Search for contact email: "
-                read EMAIL
+                read -p "Search for contact email: " EMAIL
                 done;;
 	esac
 done
-line1=$(grep -i "$NAME" database.txt | grep -i "$ADDRESS" | grep -i "$PHONE_NUMBER" | grep -i "$EMAIL")
 
-OIFS=$IFS
-IFS="
-"
-count=0
-for this in $line1
-do
-#echo "COUNT: $count"
-    IFS=":"
-    read -ra x <<< "${this}"
-    read -ra x <<< "$this"
-#count=${#x[@]}
-#count2=0
-#while [[ $count2 -lt $count ]]
-#do
-    echo "Name: ${x[count2]}"
-    echo "Address: ${x[count2+1]}"
-    echo "Phone Number: ${x[count2+2]}"
-    echo "Email: ${x[count2+3]}"
-    echo
-#count2=$count2+4
-#done
-IFS="
-"
-done
-IFS=$OIFS
+contactInfo=$(grep -i "$NAME" database.txt | grep -i "$ADDRESS" | grep -i "$PHONE_NUMBER" | grep -i "$EMAIL")
+
+if [[ ${#contactInfo} != 0 ]]
+	then
+
+	OIFS=$IFS
+	IFS="
+	"
+	count=0
+	for this in $contactInfo
+	do
+	#echo "COUNT: $count"
+	    IFS=":"
+	    read -ra x <<< "${this}"
+	    read -ra x <<< "$this"
+	#count=${#x[@]}
+	#count2=0
+	#while [[ $count2 -lt $count ]]
+	#do
+		echo -e "-----------------------------\nContact Information\n"
+	    echo "Name: ${x[count2]}"
+	    echo "Address: ${x[count2+1]}"
+	    echo "Phone Number: ${x[count2+2]}"
+	    echo "Email: ${x[count2+3]}"
+	    echo -e "-----------------------------\n"
+	#count2=$count2+4
+	#done
+	IFS="
+	"
+	done
+	IFS=$OIFS
+
+else
+	echo -e "Error: One or more fields did not exist in the database.\nTry simplying your search.\n";	
+fi
 
 # TODO
 # check for multiple entries, if so prompt for unique identifer (email)
