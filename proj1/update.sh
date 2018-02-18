62dc1d8bd25976490378c34d426ab1fb70b7a585
wcj1 <36248206+wcj1@users.noreply.github.com>
2018-02-18 13:29:30 -0600

    Update update.sh

--- proj1/update.sh
+++ proj1/update.sh
@@ -1,45 +1,60 @@
 #!/bin/bash
 # Update functionality
 
-echo -e "What would you like to update?\n"
+DATABASE="database.txt"
 
-echo -e " (a) Name\n" "(b) Address\n" "(c) Phone Number\n" "(d) Email\n"
+echo -e "Please enter the name of the record you would like to update.\n"
+read search
 
-read input
+LINE=$(grep -in "${search}" ${DATABASE})
+echo $LINE
+IFS=':'
+set $LINE
+NUM=$1
+NAME=$2
+ADDRESS=$3
+PHONE_NUMBER=$4
+EMAIL=$5
 
-	case $input in
-		"a"|"A"|"1")
-			read -p "Input current Name\n" NAME
-			if [grep -F "$NAME"]; then 
-				read -p "What would you like to change it to?\n" new_var
-			else
-				echo "No one by that name in this database.\n"
-			fi;;
-		"b"|"B"|"2")
-			read -p "Input current Address\n" ADDRESS
-			if [grep -F "$ADDRESS"]; then 
-				read -p "What would you like to change it to?\n" new_var
-			else
-				echo "No one with that address in this database.\n"
-			fi;;
-		"c"|"C"|"3")
-			read -p "Input current Phone Number\n" PHONE_NUMBER
-			if [grep -F "$PHONE_NUMBER"]; then 
-				read -p "What would you like to change it to?\n" new_var
-			else
-				echo "No one with that phone number in this database.\n"
-			fi;;
-		"d"|"D"|"4")
-			read -p "Input current Email\n" EMAIL
-			if [grep -F "$NAME"]; then 
-				read -p "What would you like to change it to?\n" new_var
-			else
-				echo "No one with that email in this database.\n"
-			fi
-			break;;
-		*) echo -e '\t\t('!') INVALID CHOICE ('!')';;	
-	esac
+while true; do
 
-#$grep -v "$var" database.txt >> updated_db.txt
+	echo -e "What would you like to update about this record?\n"
 
-echo -e "updating existing record..\n"
+	echo -e " (a) Name\n" "(b) Address\n" "(c) Phone Number\n" "(d) Email\n (e) Exit\n"
+
+	read MENU
+
+		case $MENU in
+			"a"|"A"|"1")
+				read -p "Input updated Name: " NAME
+				;;
+			"b"|"B"|"2")
+				read -p "Input updated Address: " ADDRESS
+				;;
+			"c"|"C"|"3")
+				read -p "Input updated Phone Number: " PHONE_NUMBER
+				;;
+			"d"|"D"|"4")
+				read -p "Input updated Email: " EMAIL
+				;;
+			"e"|"E"|"5")
+				echo -e "Exiting\n"
+ 				break
+				;;
+			*) 
+				echo "INVALID CHOICE"
+				;;	
+		esac
+		done
+		MENU=' '
+
+	grep -v "${search}" ${DATABASE} > temp && mv temp ${DATABASE}
+
+	echo "$NAME:$ADDRESS:$PHONE_NUMBER:$EMAIL" >> ${DATABASE}
+
+
+	echo -e "Updating existing record.\n"
+
+	IFS=' '
+
+done 
