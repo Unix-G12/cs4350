44d4c077912b2104905cba76fb59378d10a62b60
Maria <23283205+mcsanchez093@users.noreply.github.com>
2018-02-20 02:05:49 -0600

    another update

--- proj1/remove.sh
+++ proj1/remove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Author : Maria Sanchez
-# Remove functionality
+#Remove functionality
 
 
 DATABASE="database.txt"
@@ -11,12 +11,11 @@ LINE=$(grep -inm 1 -e "${REMOVE}" ${DATABASE})
 if [ "$LINE" != " " ];
     then
         RECORDS=$(grep -ci "${REMOVE}" ${DATABASE})
-fi
-
-if [[ "$REMOVE" == [^a-zA-Z\ ] ]];
-    then
-        echo -e " Invalid input. Please enter characters A-Z only "
-        ./db_menu.sh
+    if [[ "$REMOVE" == [^a-zA-Z\ ] ]];
+        then
+            echo -e " Invalid input. Please enter characters A-Z only "
+            ./db_menu.sh
+    fi
 fi
 
 if [ $RECORDS ] && [ $RECORDS -gt 1 ];
@@ -25,21 +24,20 @@ if [ $RECORDS ] && [ $RECORDS -gt 1 ];
         echo -e " \nPlease try again and enter a last name.\n "
         read -p " Enter the email to be removed: " EMAIL
         LINE=$(grep -inm 1 -e "${EMAIL}" ${DATABASE})
-        if [[ "$EMAIL" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$ ]]
+    if [[ "$EMAIL" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$ ]]
         then
             grep -v "${EMAIL}" ${DATABASE} > temp && mv temp ${DATABASE}
             echo -e " \nRecord for "$EMAIL" has been removed\n "
         else
-            echo -e " No record was found! "
+            echo -e "No record was found!"
             ./db_menu.sh
-        fi
-
+    fi
 elif [ "$LINE" != "" ];
     then
         grep -v "${REMOVE}" ${DATABASE} > temp && mv temp ${DATABASE}
         echo -e " \nRecord for "$REMOVE" has been removed\n "
     else
-        echo -e " No record was found.!"
+        echo -e "No record was found!"
         ./db_menu.sh
 fi
 
