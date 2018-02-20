7a83d6eed786c396a6fe810c52a375e7f578c426
david crowe <dac183@txstate.edu>
2018-02-20 00:24:29 -0600

    increased robustness
    loops until proper input.. though occasionally deletes entire database

--- proj1/update.sh
+++ proj1/update.sh
@@ -1,45 +1,72 @@
 #!/bin/bash
 # Update functionality
 
-echo -e "What would you like to update?\n"
+DATABASE="database.txt"
+MULTIPLE="FALSE"
+minEmailCount=5
+MENU="x"
+read -p "Please enter the name of the record you would like to update: " search
 
-echo -e " (a) Name\n" "(b) Address\n" "(c) Phone Number\n" "(d) Email\n"
+COUNT=$(grep -ci "${search}" ${DATABASE})
 
-read input
+if [ ${COUNT} -gt 1 ]; then
+	read -p "Found more than one matching record. Please enter contact's email: " UPDATE_EMAIL
 
-	case $input in
+elif [ ${COUNT} -eq 0 ]; then
+	echo -e "Could not find matching record. Please input full name.\n"
+	./update.sh
+else
+	echo -e "Contact found:"
+	LINE=$(grep -i "${search}" ${DATABASE})
+	echo -e "$LINE\n"
+	IFS=':'
+	set $LINE
+	NUM=$1
+	NAME=$2
+	ADDRESS=$3
+	PHONE_NUMBER=$4
+	EMAIL=$5
+fi	
+
+while true; 
+do
+
+until [[ $MENU =~ ^[a-e]*$ ]]
+do 
+
+	echo -e "What would you like to update about this record?\nEnter up to 4 choices with no spaces (e.g. bcd)"
+
+	echo -e " (a) Name\n" "(b) Address\n" "(c) Phone Number\n" "(d) Email\n (e) Nothing\n"
+
+	read -p "Choice: " MENU
+done
+for ((m=0; m<${#MENU}; m++)); do
+	case $MENU in
 		"a"|"A"|"1")
-			read -p "Input current Name\n" NAME
-			if [grep -F "$NAME"]; then 
-				read -p "What would you like to change it to?\n" new_var
-			else
-				echo "No one by that name in this database.\n"
-			fi;;
+			read -p "Input updated Name: " NAME
+			;;
 		"b"|"B"|"2")
-			read -p "Input current Address\n" ADDRESS
-			if [grep -F "$ADDRESS"]; then 
-				read -p "What would you like to change it to?\n" new_var
-			else
-				echo "No one with that address in this database.\n"
-			fi;;
+			read -p "Input updated Address: " ADDRESS
+			;;
 		"c"|"C"|"3")
-			read -p "Input current Phone Number\n" PHONE_NUMBER
-			if [grep -F "$PHONE_NUMBER"]; then 
-				read -p "What would you like to change it to?\n" new_var
-			else
-				echo "No one with that phone number in this database.\n"
-			fi;;
+			read -p "Input updated Phone Number: " PHONE_NUMBER
+			;;
 		"d"|"D"|"4")
-			read -p "Input current Email\n" EMAIL
-			if [grep -F "$NAME"]; then 
-				read -p "What would you like to change it to?\n" new_var
-			else
-				echo "No one with that email in this database.\n"
-			fi
-			break;;
-		*) echo -e '\t\t('!') INVALID CHOICE ('!')';;	
+			read -p "Input updated Email: " EMAIL
+			;;
+		"e"|"E"|"5")
+			echo -e "Database unchanged.\n"
+			./db_menu.sh
+			break
+			;;
+		*) 
 	esac
+done		
+	grep -v "${search}" ${DATABASE} > temp && mv temp ${DATABASE}
 
-#$grep -v "$var" database.txt >> updated_db.txt
+	echo "$NAME:$ADDRESS:$PHONE_NUMBER:$EMAIL" >> ${DATABASE}	
 
-echo -e "updating existing record..\n"
+	echo -e "Updating existing record.\n"
+	IFS=' '
+	MENU=' '
+done 
